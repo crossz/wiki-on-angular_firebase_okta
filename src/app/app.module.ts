1ac3762c13619943393e0ab0a616227725472852
郑欣 <zhengxin@zhengxindeiMac.local>
2019-09-11 15:13:27 +0800

    :fire: angular works, before replacing {{content}}.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8uDBiuKh6xDfBVV4ZM6ZdIx2Tf28_2lw",
+  authDomain: "myngwiki.firebaseapp.com",
+  databaseURL: "https://myngwiki.firebaseio.com",
+  projectId: "myngwiki",
+  storageBucket: "myngwiki.appspot.com",
+  messagingSenderId: "22899147088",
+  appId: "1:22899147088:web:1ef707ccc0cb455bac6c55"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AuthRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
