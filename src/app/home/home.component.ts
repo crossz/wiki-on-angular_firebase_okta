9c29bbcb79cc4dfb857d7f5154d6bc41e2f475fe
郑欣 <zhengxin@zhengxindeiMac.local>
2019-09-30 12:57:21 +0800

    :tada: prepared for start to replacing angularFireStore.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,6 +43,21 @@ export class HomeComponent implements OnInit {
     }
 
     const doc = this.db.collection('pages').doc(slug).get();
+
+    /**
+     * in the file of @angular/fire/firestore/document/document.js:
+     * this.ref is the object represented by 'pages', 
+     * this.options is null. where the get() is from Map.get( ) In JavaScript, and it can be anything, even null or ''.
+     * 
+     * Therefore, the final var returned is actually from 'from()'.
+     * from() as a Rxjs function, is to, turn an array, promise, or iterable into an observable.
+     * Ref: https://www.learnrxjs.io/operators/creation/from.html
+     * 
+     */
+    // AngularFirestoreDocument.prototype.get = function (options) {
+    //   return from(this.ref.get(options)).pipe(runInZone(this.afs.scheduler.zone));
+    // };
+
     this.subs = doc.subscribe((snapshot) => {
       const page = snapshot.data();
       if (!page) {
